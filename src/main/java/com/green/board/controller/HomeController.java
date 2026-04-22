7f8537426e9684ec406137c667808a02095eef17
Siyoung0731 <azxx0731@gmail.com>
2026-04-22 15:47:26 +0900

    second commit

--- src/main/java/com/green/board/controller/HomeController.java
+++ src/main/java/com/green/board/controller/HomeController.java
@@ -8,10 +8,10 @@ public class HomeController {
 	
 	@RequestMapping("/")
 	public String home() {
-		return "home";
+		return "home"; // jsp 파일을 찾는다
 	}
 	@RequestMapping("/test")
-	@ResponseBody
+	@ResponseBody  // 서버가 data(html) 를 내려보낸다
 	public String test() {
 		return "<h2>Test 입니다</h2>";
 	}
